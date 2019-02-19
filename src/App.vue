ace4a4f1464d5f05b3ee5de532d30cb1823cc061
Arne De Smedt <arnedesmdt@gmail.com>
2019-02-19 13:24:22 +0100

    remove fixed body

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <vue-ads-context
-            :toggle-on-hover="true"
+            :toggle-on-hover="false"
             class="vue-ads-absolute"
             style="left: 267px; top: 456px;"
             horizontal-position="right-right"
@@ -27,8 +27,7 @@ export default {
 </script>
 
 <style>
-    body{
-        position: fixed;
+    html,body {
         width: 100%;
         height: 100%;
     }
